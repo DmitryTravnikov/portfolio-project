f5b5c7cadc6f0df32e8fb3f51d3097bfe92edbdb
Dmitry Travnikov <travnikov@outlook.com>
2022-08-02 16:18:29 +0300

    preloader js is modified

--- src/components/__preloader/preloader.js
+++ src/components/__preloader/preloader.js
@@ -1,5 +1,5 @@
 import { cLog } from '../../index.js';
 
 $(window).on('load', function () {
-  $('.preloader').delay(2000).fadeOut().end().fadeOut('slow');
+  $('.preloader').delay(500).fadeOut().end().fadeOut('slow');
 });
